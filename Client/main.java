2250c25269226d8a9d12b5c24223852149d7ffee
Ginzo Milani <lf2owner@hotmail.com>
2019-01-24 15:48:08 -0800

    Started program

---
+++ Client/main.java
@@ -0,0 +1,16 @@
+/******************************************************************************
+ *  Compilation:  javac HelloWorld.java
+ *  Execution:    java HelloWorld
+ *
+ *  Client project for Drawsomething game.
+ *
+ ******************************************************************************/
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+
+}
