54f66115e4606eb057242910e36a7d24b916967a
Ginzo Milani <lf2owner@hotmail.com>
2019-01-24 16:55:46 -0800

    Update to include client

--- main.java
+++ main.java
@@ -28,10 +28,14 @@ public class main {
 	 * 
 	 */
     public static void main(String[] args) {
-        // Prints "Hello, World" to the terminal window.
         Scanner input = new Scanner (System.in);
         System.out.println("Please pick a mode:\n[C]lient\n[S]erver");
         String mode = input.next(); // Get what the user types.
+        boolean isClient = mode.toLowerCase() == "c" ? true : false;
+        if(isClient)
+        	runClient();
+        else
+        	runServer();
     }
 
 }
