c06652f4b2ec50cd43a4f14cd0f2d177bd443bac
Milani <milanip@student.douglascollege.ca>
2019-03-07 10:47:30 -0800

    Added drawing logic and saving.

--- DrawingPage.java
+++ DrawingPage.java
@@ -1,41 +1,265 @@
-import java.awt.EventQueue;
+    import java.awt.Graphics;
+    import java.awt.*;
+    import java.awt.event.*;
+import java.awt.image.BufferedImage;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 
-import javax.swing.JFrame;
+import javax.swing.*;
 
-public class DrawingPage {
+import com.sun.image.codec.jpeg.JPEGCodec;
+import com.sun.image.codec.jpeg.JPEGImageEncoder;
 
-	private JFrame frame;
+    public class DrawingPage{
+        public static void main(String[] args){     
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main() {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					DrawingPage window = new DrawingPage();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+    JFrame frame = new JFrame("Draw Something");    
 
-	/**
-	 * Create the application.
-	 */
-	public DrawingPage() {
-		initialize();
-	}
+    Container content = frame.getContentPane();
 
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 752, 715);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
+    content.setLayout(new BorderLayout());      
 
+    final PadDraw drawPad = new PadDraw();      
+
+    content.add(drawPad, BorderLayout.CENTER);
+
+
+    JPanel panel = new JPanel();
+
+    panel.setPreferredSize(new Dimension(100, 68));
+    panel.setMinimumSize(new Dimension(100, 68));
+    panel.setMaximumSize(new Dimension(100, 68));
+
+            JButton twoX = new JButton ("2");
+            twoX.addActionListener(new ActionListener(){
+
+                public void actionPerformed(ActionEvent e){
+            drawPad.clear();
+        }
+    });             
+
+
+    JButton yellowButton = new JButton("YELLOW");
+    yellowButton.setForeground(new Color(204, 204, 0));
+
+    yellowButton.addActionListener(new ActionListener(){
+        public void actionPerformed(ActionEvent e){
+            drawPad.yellow();
+        }
+
+    });
+
+
+    JButton greenButton = new JButton("GREEN");
+    greenButton.setForeground(new Color(0, 204, 0));
+
+    greenButton.addActionListener(new ActionListener(){
+        public void actionPerformed(ActionEvent e){
+            drawPad.green();
+        }
+    });
+
+    JButton redButton = new JButton("RED");
+    redButton.setForeground(new Color(255, 0, 0));
+
+    redButton.addActionListener(new ActionListener(){
+        public void actionPerformed(ActionEvent e){
+            drawPad.red();
+        }
+    });
+
+    JButton blueButton = new JButton("BLUE");
+    blueButton.setForeground(Color.BLUE);
+
+    blueButton.addActionListener(new ActionListener(){
+        public void actionPerformed(ActionEvent e){
+            drawPad.blue();
+        }
+    });
+
+    JButton blackButton = new JButton("BLACK");
+
+    blackButton.addActionListener(new ActionListener(){
+        public void actionPerformed(ActionEvent e){
+            drawPad.black();
+        }
+    });
+    
+    JButton sendButton = new JButton("SEND");
+
+    sendButton.addActionListener(new ActionListener(){
+        public void actionPerformed(ActionEvent e){
+        	drawPad.saveComponentAsJPEG(drawPad,"C:/test.jpg");
+        }
+    });
+    greenButton.setPreferredSize(new Dimension(80, 20));
+    redButton.setPreferredSize(new Dimension(80, 20));
+    yellowButton.setPreferredSize(new Dimension(80, 20));
+    blueButton.setPreferredSize(new Dimension(80, 20));
+    blackButton.setPreferredSize(new Dimension(80,20));
+    sendButton.setPreferredSize(new Dimension(100,20));
+
+
+    panel.add(blackButton);
+    panel.add(blueButton);
+    panel.add(redButton);
+    panel.add(greenButton);
+    panel.add(yellowButton);
+    panel.add(sendButton);
+
+
+    content.add(panel, BorderLayout.NORTH);
+
+    JRadioButton rdbtnPx = new JRadioButton("3 px");
+    panel.add(rdbtnPx);
+
+    JRadioButton rdbtnPx_1 = new JRadioButton("5 px");
+    panel.add(rdbtnPx_1);
+
+    JRadioButton rdbtnPx_2 = new JRadioButton("12 px");
+    panel.add(rdbtnPx_2);
+
+
+    ButtonGroup bg = new ButtonGroup();
+    bg.add(rdbtnPx);
+    bg.add(rdbtnPx_1);
+    bg.add(rdbtnPx_2);
+
+    rdbtnPx.addActionListener(new ActionListener(){
+        public void actionPerformed(ActionEvent e){
+            drawPad.small();
+        }
+    });
+    rdbtnPx_1.addActionListener(new ActionListener(){
+        public void actionPerformed(ActionEvent e){
+            drawPad.medium();
+        }
+    });
+    rdbtnPx_2.addActionListener(new ActionListener(){
+        public void actionPerformed(ActionEvent e){
+            drawPad.big();
+        }
+    });
+
+    JButton clearButton = new JButton("Clear");
+    clearButton.setBackground(new Color(255, 255, 255));
+    clearButton.setFont(UIManager.getFont("TextArea.font"));
+
+    clearButton.addActionListener(new ActionListener(){
+        public void actionPerformed(ActionEvent e){
+            drawPad.clear();
+        }
+    });
+    panel.add(clearButton);
+
+    frame.setSize(500, 440);
+
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    frame.setVisible(true);
+
+}
+}
+
+
+class PadDraw extends JComponent{
+
+private Image image;    
+private Graphics2D graphics2D;  
+private int currentX , currentY , oldX , oldY ;
+
+public PadDraw(){
+    setDoubleBuffered(false);
+    addMouseListener(new MouseAdapter(){
+        public void mousePressed(MouseEvent e){
+            oldX = e.getX();
+            oldY = e.getY();
+        }
+    });
+
+    addMouseMotionListener(new MouseMotionAdapter(){
+        public void mouseDragged(MouseEvent e){
+            currentX = e.getX();
+            currentY = e.getY();
+            if(graphics2D != null)
+            graphics2D.drawLine(oldX, oldY, currentX, currentY);
+            repaint();
+            oldX = currentX;
+            oldY = currentY;
+        }
+
+    });
+
+}   
+
+public void paintComponent(Graphics g){
+    if(image == null){
+        image = createImage(getSize().width, getSize().height);
+        graphics2D = (Graphics2D)image.getGraphics();
+        graphics2D.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        clear();
+
+    }
+    g.drawImage(image, 5, 5, null);
+}
+
+public void clear(){
+    graphics2D.setPaint(Color.white);
+    graphics2D.fillRect(0, 0, getSize().width, getSize().height);
+    graphics2D.setPaint(Color.black);
+    repaint();
+}
+
+public void red(){
+    graphics2D.setPaint(Color.red);
+    repaint();
+}
+
+public void black(){
+    graphics2D.setPaint(Color.black);
+    repaint();
+}
+
+public void yellow(){
+    graphics2D.setPaint(Color.yellow);
+    repaint();
+}
+
+public void blue(){
+    graphics2D.setPaint(Color.blue);
+    repaint();
+}
+
+public void green(){
+    graphics2D.setPaint(Color.green);
+    repaint();
+}
+public void small(){
+    graphics2D.setStroke(new BasicStroke(1));;
+}
+public void medium(){
+    graphics2D.setStroke(new BasicStroke(5));;
+}
+public void big(){
+    graphics2D.setStroke(new BasicStroke(12));;
+}
+public void sendImage(Component g) {
+	//connect to server, convert to binary and send.
+}
+public void saveComponentAsJPEG(Component myComponent, String filename) {
+    Dimension size = myComponent.getSize();
+    BufferedImage myImage = 
+      new BufferedImage(size.width, size.height,
+      BufferedImage.TYPE_INT_RGB);
+    Graphics2D g2 = myImage.createGraphics();
+    myComponent.paint(g2);
+    try {
+      OutputStream out = new FileOutputStream(filename);
+      JPEGImageEncoder encoder = JPEGCodec.createJPEGEncoder(out);
+      encoder.encode(myImage);
+      out.close();
+    } catch (Exception e) {
+      System.out.println(e); 
+    }
+  } 
 }
