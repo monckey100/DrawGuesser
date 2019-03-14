1714a3ab998a91697bf107126c11c52df42de849
Milani <milanip@student.douglascollege.ca>
2019-03-14 12:22:25 -0700

    Added cleaner classes and structure, client.java is NOT finished.

--- Client.java
+++ Client.java
@@ -5,9 +5,7 @@ class Client
 {
 	static BufferedReader inFromUser;
 	static DatagramSocket clientSocket = null;
-	static InetAddress IPAddress = null;
-	
-    
+ 
    public static void main(String args[]) throws Exception
    {
 	   	// just for testing
@@ -22,74 +20,96 @@ class Client
 	   String databasePackage;
 	   byte[] receiveData = new byte[1024];
 	   inFromUser = new BufferedReader(new InputStreamReader(System.in));
-
-			try {
-				clientSocket = new DatagramSocket();
-			} catch (SocketException e2) {
-				// TODO Auto-generated catch block
-				e2.printStackTrace();
-			}
-			
+	   clientConnection myConn = new clientConnection("localhost");
 		      
-			try {
-				IPAddress = InetAddress.getByName("localhost");
-			} catch (UnknownHostException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
 			// Concatenate the request
 			String request = requestType+","+userRequest;
 			// Send the request
-			sendData(request);			     
+			myConn.sendData(request);			     
 						
-		    databasePackage = receiveData(100);
+		    databasePackage = myConn.receiveData(100);
 		      		      
 		    System.out.println("FROM SERVER:" + databasePackage);
 		    clientSocket.close();
 	   
-	   
+	   /*
+	  BufferedReader inFromUser =
+      new BufferedReader(new InputStreamReader(System.in));
+      DatagramSocket clientSocket = new DatagramSocket();
+      InetAddress IPAddress = InetAddress.getByName("localhost");
+      byte[] sendData = new byte[1024];
+      byte[] receiveData = new byte[1024];
+      String sentence = inFromUser.readLine();
+      sendData = sentence.getBytes();
+      DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
+      clientSocket.send(sendPacket);
+      DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+      clientSocket.receive(receivePacket);
+      String modifiedSentence = new String(receivePacket.getData());
+      System.out.println("FROM SERVER:" + modifiedSentence);
+      clientSocket.close();
+	    */
 	   return databasePackage;
 	   
    }
-   public static void sendData( String request) {
-	  
-	      byte[] sendData = new byte[1024];
-	      sendData = request.getBytes();
-	      DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
-	      try {
-			clientSocket.send(sendPacket);
-		} catch (IOException e) {
+
+
+}
+class clientConnection {
+	InetAddress IPAddress = null;
+	DatagramSocket clientSocket = null;
+	public clientConnection(String Name) {
+		try {
+			clientSocket = new DatagramSocket();
+		} catch (SocketException e2) {
+			// TODO Auto-generated catch block
+			e2.printStackTrace();
+		}
+		try {
+			IPAddress = InetAddress.getByName(Name);
+		} catch (UnknownHostException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-   }
-  
-   public static String receiveData(int waitTime) {
-	   byte[] receiveData = new byte[1024];
-	   String databasePackage;
-	   
-	   DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-	// Wait for a reply
-				for( int i = 0; i < 10 ; i++) {
-					try {
-						Thread.sleep(waitTime);
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
+	}
+	public void sendData( String request) {
+			  
+		      byte[] sendData = new byte[1024];
+		      sendData = request.getBytes();
+		      DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
+		      try {
+				clientSocket.send(sendPacket);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	}
+	public String receiveData(int waitTime) {
+		   byte[] receiveData = new byte[1024];
+		   String databasePackage;
+		   
+		   DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+		// Wait for a reply
+					for( int i = 0; i < 10 ; i++) {
+						try {
+							Thread.sleep(waitTime);
+						} catch (InterruptedException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
 					}
-				}
-									   
-	      
-	    try {
-			clientSocket.receive(receivePacket);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	      databasePackage = new String(receivePacket.getData());
-	      
-	      return databasePackage;
-   }
+										   
+		      
+		    try {
+				clientSocket.receive(receivePacket);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		      databasePackage = new String(receivePacket.getData());
+		      
+		      return databasePackage;
+	}
 }
 
 
--- Server.java
+++ Server.java
@@ -12,18 +12,40 @@ class Server
    public static void main(String args[]) throws Exception
       {
 	   connectionHandler myConn = new connectionHandler();
-	   // OPen connection to the database
+	   // Open connection to the database
 	   JDBC2 jdbc = new JDBC2();
             while(true)
               {                       
-                 String sentence = myConn.getPackets();
-                 if(sentence != null) {
-                	 System.out.println("RECEIVED: " + sentence); 
-                	 // Get request and request type
-                	 String[] requestArray = sentence.split(",");
+                 String packet = myConn.getPackets();
+                 if(packet != null) {
+                	 System.out.println("RECEIVED: " + packet); 
+                	 myConn.parsePacket(packet);
+                	 //String dbResult = jdbc.getResult(requestArray[0],requestArray[1]);
+                	 if(myConn.getType() == "LOGIN") {
+                		 //We assume getData will be {username,password}
+	                	 myConn.setType("LOGIN");
+	                	 String username = myConn.getData()[0];
+	                	 String password = myConn.getData()[1];
+	                	 String[] sendInfo = null;
+	                	 /*
+	                	 if(jdbc.checkLogin(username,password)) {
+	                		 sendInfo[0] = "Successful";
+	                		 sendInfo[1] = jdbc.getEXP(username);
+	                		 sendInfo[2] = jdbc.getLevel(username);
+	                	 } else {
+	                		 sendInfo[0] = "Failed";
+	                	 }*/
+	                	 //myConn.setData(new String[]{"My","Data"});
+	                	 myConn.setData(sendInfo);
+                	 } else {
+                		 myConn.setType("ERROR");
+                		 myConn.setData(new String[] {"Error: Could not find Datatype!"});
+                	 }
+                	 //Sends raw data how we want it to.
+                	 //myConn.sendPacket(dbResult);
                 	 
-                	 String dbResult = jdbc.getResult(requestArray[0],requestArray[1]);
-                	 myConn.sendPacket(dbResult);
+                	 //Sends setType + setData to server.
+                	 myConn.sendPacket(myConn.packageData());
                   }
 
                }
@@ -35,6 +57,9 @@ class connectionHandler {
 	DatagramSocket serverSocket;
 	InetAddress IPAddress;
 	int port;
+	String[] requestArray;
+	String datatype;
+	String[] datastuff;
 	public connectionHandler() {
 		 try {
 			serverSocket = new DatagramSocket(9876);
@@ -81,5 +106,28 @@ class connectionHandler {
 			e.printStackTrace();
 			return false;
 		}
+	}
+	public void parsePacket(String mPacket) {
+		 requestArray = mPacket.split(",");
+	}
+	public String getType() {
+		return requestArray[0];
+	}
+	public String[] getData() {
+		//remove first character [
+		String m = requestArray[1].substring(1); 
+		//remove last character ]
+		m = m.substring(0, m.length() - 1);
+		return m.split(","); 
+	}
+	//Sending to client
+	public void setType(String type) {
+		datatype = type;
+	}
+	public void setData(String[] data) {
+		datastuff = data;
+	}
+	public String packageData() {
+		return datatype + "," + datastuff.toString();
 	}
 }
