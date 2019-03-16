9bbca7601ee58b1453fc56e8548ee96808ac91c6
kevintsan97 <taitsan2406@gmail.com>
2019-03-16 10:30:49 -0700

    Group up prepared statement, query and 1 method for entire result return

--- JDBC2.java
+++ JDBC2.java
@@ -3,11 +3,16 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class JDBC2 {
 
 	static String dbURL = "jdbc:sqlserver://localhost;"+ "instanceName=SQLEXPRESS;databaseName=DrawGuesser;user=sa;password=csis3300";
 	static Connection conn = null;
+	public static boolean check = false;
+	// Keep the username when it is valid
+	static String validUserName;
+
 	public static void openConnection() {
 		
 		try { 			
@@ -30,14 +35,33 @@ public class JDBC2 {
 		
 	}
 	
-	public static PreparedStatement getUserNameAndPassword(String query,String userName, String password) {
+	public static String getQuery(String requestType) {
+		String result = "";
+		switch(requestType) {
+		case "LOGIN":
+			result = "SELECT userName,_XP,_LEVEL FROM _USER WHERE ? = ?";
+			break;
+		// Will continue on later
+		}
+		
+		
+		
+		return result;
+	}
+	
+	public static PreparedStatement getPreparedStatement(String query,String ... args) {
 		PreparedStatement prepStmt1 = null;		
 
 		try {
 			if(conn !=null) {
 				prepStmt1 = conn.prepareStatement(query);
-				prepStmt1.setString(1, userName);
-				prepStmt1.setString(2, password);
+				
+				for(int i = 0 ; i < args.length ; i++) {
+					prepStmt1.setString(i+1, args[i]);
+					
+				
+				}
+				
 			}
 			
 			
@@ -48,45 +72,31 @@ public class JDBC2 {
 		return prepStmt1;
 	}
 	
-	public static String getResult(String typeOfRequest, String request) {
-		String result="";
-		openConnection();
-		String query="SELECT * FROM _USER WHERE ? = ?";
 
+	
+	public static ArrayList getData(String requestType,String ... args) {
+		// Open the connection
+		openConnection();
+
+		// Get query base on type
+		String query= getQuery(requestType);
+		// Initialize prepared statement
+		PreparedStatement prepStatement =null;
+		// Initialize array list
+		ArrayList<String> resultList = new ArrayList<>();
 		try {
 			if (conn != null) {
-				PreparedStatement prepStatement = getUserNameAndPassword(query,typeOfRequest,request);
-				ResultSet rs = prepStatement.executeQuery();
 				
-				while ( rs.next() ) {
-					result = rs.getString("ID");
-				}
-							
-			}
-		}
-		catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	
-		closeConnection();
-		return result;
-		
-	}
-	
-	public static boolean checkLogin(String userName, String password) {
-		boolean check =false;
-		
-		openConnection();
-		String query="SELECT userName FROM _USER WHERE userName = ? and _Password =? ";
-
-		try {
-			if (conn != null) {
-				PreparedStatement prepStatement = getUserNameAndPassword(query,userName,password);
+				prepStatement = getPreparedStatement(query,args);
 				ResultSet rs = prepStatement.executeQuery();
 				
 				if ( rs.next() ) {
 					check =true;
+					validUserName = rs.getString("userName");
+				}
+				for( int i = 0 ; i < args.length ; i++) {
+					// Items's index will be the same as the columns in select query
+					resultList.add(rs.getString(args[i]));
 				}
 							
 			}
@@ -97,7 +107,45 @@ public class JDBC2 {
 		}
 	
 		closeConnection();
-		return check;
+		return resultList;
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*	public static String getResult(String typeOfRequest, String request) {
+String result="";
+openConnection();
+String query="SELECT * FROM _USER WHERE ? = ?";
+
+try {
+	if (conn != null) {
+		PreparedStatement prepStatement = getUserNameAndPassword(query,typeOfRequest,request);
+		ResultSet rs = prepStatement.executeQuery();
+		
+		while ( rs.next() ) {
+			result = rs.getString("ID");
+		}
+					
+	}
+}
+catch (SQLException e) {
+	// TODO Auto-generated catch block
+	e.printStackTrace();
+}
+
+closeConnection();
+return result;
+
+}*/
