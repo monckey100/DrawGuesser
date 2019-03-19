4992b78a325d316508459b4923bd0097d13ede2a
kevintsan97 <taitsan2406@gmail.com>
2019-03-18 21:35:36 -0700

    sign up and login page finish!

--- JDBC2.java
+++ JDBC2.java
@@ -35,15 +35,15 @@ public class JDBC2 {
 		
 	}
 	
-	public static String getQuery(String requestType,String uniqueUserName, String uniqueEmail) {
+	public static String getQuery(String requestType,String ...args ) {
 		String result = "";
 		switch(requestType) {
 		case "LOGIN":
-			result = "SELECT userName,_XP,_LEVEL FROM _User WHERE userName = ? AND _Password = ?";
+			result = "SELECT _Exp,_Level FROM _User WHERE userName = ? AND _Password = ?";
 			break;
-		// Will continue on later
+
 		case "SIGNUP":
-			result =  "IF NOT EXISTS ( SELECT * FROM _User WHERE Email = '" +uniqueEmail+"' OR userName = '"+ uniqueUserName+"' ) "
+			result =  "IF NOT EXISTS ( SELECT * FROM _User WHERE Email = '" +args[0]+"' OR userName = '"+ args[1]+"' ) "
 					+ " BEGIN "
 					+ " INSERT INTO _USER (userName,_Password,Fname,Lname,Email,_Level,_Exp) "
 					+ " VALUES(?,?,?,?,?,1,0) END "
@@ -82,16 +82,18 @@ public class JDBC2 {
 	
 
 	
-	public static String[] getData(String requestType,String ... args) {
+	public static String[] getData(String requestType,String[] inforArray,String ... args) {
 		// Open the connection
 		openConnection();
 
 		// Get query base on type, userNAme and email must be unique
-		String query= getQuery(requestType,args[0],args[4]);
+		String query= getQuery(requestType);
 		// Initialize prepared statement
 		PreparedStatement prepStatement =null;
 		// Initialize array 
-		String[] resultArray = new String[args.length];
+		// array length + 1 since we need the first index to indicate success or fail request
+		
+		String[] resultArray = new String[inforArray.length + 1];
 		try {
 			if (conn != null) {
 				
@@ -100,13 +102,17 @@ public class JDBC2 {
 				
 				if ( rs.next() ) {
 					check =true;
-					validUserName = rs.getString("userName");
+					validUserName = args[0];
 				}
-				if(check)
-					for( int i = 0 ; i < args.length ; i++) {
+				if(check) {						
+					resultArray [0] ="Success";
+					for( int i = 1 ; i < resultArray.length ; i++) {
 						// Items's index will be the same as the columns in select query
-						resultArray[i] = rs.getString(args[i]);
+						resultArray[i] = rs.getString(inforArray[i-1]);
 					}
+				}
+				else
+					resultArray[0] = "Fail";
 							
 			}
 		}
